9217caddaad792e51ada560e3bc328c4d208ca2b
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-03 15:35:36 +0530

    Time: 3 ms (100%), Space: 175.8 MB (15.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int pos[] = new int[nums.length/2];
-        int p=0;
-        int neg[] = new int[nums.length/2];
-        int n =0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]>0){
-                pos[p] = nums[i];
-                p++;
-            }
+        // Brute Force:
+        // int pos[] = new int[nums.length/2];
+        // int p=0;
+        // int neg[] = new int[nums.length/2];
+        // int n =0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]>0){
+        //         pos[p] = nums[i];
+        //         p++;
+        //     }
+        //     if(nums[i]<0){
+        //         neg[n] = nums[i];
+        //         n++;
+        //     } 
+        // }
+        // for(int i =0; i<pos.length; i++){
+        //     nums[2*i]= pos[i];
+        //     nums[(2*i) + 1] = neg[i];
+        // }
+        // return nums;
+
+        // Optimal code:
+        int ans[] = new int[nums.length];
+        int n = nums.length;
+        int posind =0;
+        int negind=1;
+        for(int i=0; i<n; i++){
             if(nums[i]<0){
-                neg[n] = nums[i];
-                n++;
-            } 
+                ans[negind] = nums[i];
+                negind += 2;
+            }
+            else{
+                ans[posind] = nums[i];
+                posind +=2;
+            }
         }
-        for(int i =0; i<pos.length; i++){
-            nums[2*i]= pos[i];
-            nums[(2*i) + 1] = neg[i];
-        }
-        return nums;
+        return ans;
     }
 }
