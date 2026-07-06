7c849e4994d959f7660e025cf9e9908f40a1453f
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-06 11:44:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.44%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,39 +1,38 @@
-import java.util.*;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-      int strow=0;
-      int edrow= matrix.length-1;
-      int stcol=0;
-      int edcol=matrix[0].length-1;
-      List<Integer> ans = new ArrayList<>();
+        int strow=0;
+        int stcol=0;
+        int edcol = matrix[0].length-1;
+        int edrow = matrix.length-1;
+        List<Integer> ans = new ArrayList<>();
       while(strow<=edrow && stcol<=edcol){
-        //top:
+        // top
         for(int j=stcol; j<=edcol; j++){
             ans.add(matrix[strow][j]);
         }
-        //right:
-        for(int i=strow+1; i<=edrow; i++){
-           ans.add(matrix[i][edcol]);
+        // right:
+        for(int i= strow+1; i<=edrow; i++){
+            ans.add(matrix[i][edcol]);
         }
-        //bottom:
-        for(int j=edcol-1; j>=stcol; j--){
+        // bottom:
+        for(int j =edcol-1; j>=stcol; j-- ){
             if(strow==edrow){
                 break;
             }
-           ans.add(matrix[edrow][j]);
+            ans.add(matrix[edrow][j]);
         }
-        //left
+        // left:
         for(int i=edrow-1; i>=strow+1; i--){
             if(stcol==edcol){
                 break;
             }
             ans.add(matrix[i][stcol]);
         }
-         strow++;
-         stcol++;
-         edrow--;
-          edcol--;
-      }  
+        strow++;
+        stcol++;
+        edcol--;
+        edrow--;
+      }
       return ans;
     }
 }
