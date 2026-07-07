83e990503ee25f64d47ae2f623c0120956bca99f
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-07 12:03:58 +0530

    Time: 4 ms (48.61%), Space: 44.6 MB (48.5%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+               char chi= Character.toLowerCase(ch);
+                sb.append(chi);
+            }
+        }
+        int left =0; int right = sb.length()-1;
+        while(left<=right){
+            if(sb.charAt(left) != sb.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
