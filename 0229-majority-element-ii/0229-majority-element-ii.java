1540a2b6ab118002616577b5a8432370599e97ce
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-08 14:49:14 +0530

    Time: 19 ms (11.86%), Space: 53.1 MB (16.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans =new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            if(ans.size()==0 ||  !ans.contains(nums[i]) ){
-                int count =0;
-                for(int j =0; j<nums.length; j++){
-                    if(nums[j]==nums[i]){
-                        count ++;
-                    }
-                }
-                if(count>nums.length/3){
-                    ans.add(nums[i]);
-                }
-                if(ans.size()==2){
-                    break;
-                }
+        // Bruet force: 
+        // List<Integer> ans =new ArrayList<>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(ans.size()==0 ||  !ans.contains(nums[i]) ){
+        //         int count =0;
+        //         for(int j =0; j<nums.length; j++){
+        //             if(nums[j]==nums[i]){
+        //                 count ++;
+        //             }
+        //         }
+        //         if(count>nums.length/3){
+        //             ans.add(nums[i]);
+        //         }
+        //         if(ans.size()==2){
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+        int n = nums.length;
+        HashMap<Integer, Integer> check = new HashMap<>();
+        List<Integer> is = new ArrayList<>();
+        int min = (n/3)+1;
+        for(int i=0; i<n; i++){
+            check.put(nums[i], check.getOrDefault(nums[i], 0) +1);
+            if(check.get(nums[i])==min){
+                is.add(nums[i]);
+            }
+            if(is.size()==2){
+                break;
             }
         }
-        return ans;
+        Collections.sort(is);
+        return is;
     }
 }
