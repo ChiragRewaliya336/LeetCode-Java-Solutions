8640a2c72bcfdf52685f2e326b3cb3f1d656cd8f
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-20 14:23:05 +0530

    Time: 0 ms (100%), Space: 46.1 MB (62.01%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -4,6 +4,7 @@ class Solution {
         // ans[0] = celsius + 273.15;
         // ans[1] = (celsius*1.80) + 32.00;
         // return ans;
+        // return new double[] {celsius+273.15, celsius*1.80 + 32.00};
         return new double[] {celsius+273.15, celsius*1.80 + 32.00};
     }
 }
