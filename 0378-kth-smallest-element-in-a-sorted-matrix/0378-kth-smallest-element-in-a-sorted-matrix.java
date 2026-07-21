810b23423af074f3d9635e6006cfe1bac9a1916e
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-07-21 12:56:50 +0530

    Time: 1 ms (68.29%), Space: 51.9 MB (28.61%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,12 +1,39 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                list.add(matrix[i][j]);
+        // ArrayList<Integer> list = new ArrayList<>();
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         list.add(matrix[i][j]);
+        //     }
+        // }
+        // Collections.sort(list);
+        // return list.get(k-1);
+
+        // Optimal:
+        int n = matrix.length;
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+        while(low<high){
+            int mid = low + (high-low)/2;
+            int count =0;
+            int i=n-1;
+            int j=0;
+            while(i>=0 && j<n){
+                if(matrix[i][j]>mid){
+                    i--;
+                }
+                else{
+                    count += i+1;
+                    j++;
+                }
+            }
+            if(count<k){
+                low = mid+1;
+            }
+            else{
+                high = mid;
             }
         }
-        Collections.sort(list);
-        return list.get(k-1);
+        return low;
     }
 }
