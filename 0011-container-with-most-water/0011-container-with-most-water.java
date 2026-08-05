2c80f608d5c5dca4df99ae7bd50bf1cb70cee6d3
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-08-05 18:24:10 +0530

    Time: 5 ms (82.96%), Space: 77.3 MB (69.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,33 +1,31 @@
 class Solution {
     public int maxArea(int[] height) {
-        // brute force:
-        // int maxarea =0;
-        // for(int i =0; i<height.length; i++){
-            
-        //     for(int j =i+1; j<height.length; j++){
-        //         int heig= Math.min(height[i],height[j]);
-        //        int  currar= heig*(j-i);
-        //     maxarea = Math.max(maxarea,currar);
+        // Brute fore:
+        // int maxarea = Integer.MIN_VALUE;
+        // for(int i=0; i<height.length; i++){
+        //     for(int j=i+1;j<height.length; j++){
+        //         int ht = Math.min(height[i], height[j]);
+        //         int curr = ht*(j-i);
+        //         maxarea = Math.max(curr, maxarea);
         //     }
         // }
         // return maxarea;
 
-        // optimal:
+        // Optimal:
         int l = 0;
-         int r = height.length-1;
-         int ma = 0;
-         while(l<=r){
-            int curr =0;
-            int hgt= Math.min(height[l],height[r]);
-            int ara = hgt*(r-l);
+        int r = height.length-1;
+        int ma=0;
+        while(l<=r){
+            int ht = Math.min(height[l], height[r]);
+            int curr = ht *(r-l);
             if(height[l]<height[r]){
                 l++;
             }
             else{
                 r--;
             }
-            ma = Math.max(ara,ma);
-         }
-         return ma;
+            ma = Math.max(curr, ma);
+        }
+        return ma;
     }
 }
