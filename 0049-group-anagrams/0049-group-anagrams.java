fea67136bf8f95831e9494d8f5498e0e8896eead
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-08-27 21:07:54 +0530

    Time: 7 ms (72.87%), Space: 49.9 MB (32.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // HashMap<String, List<String>> map = new HashMap<>();
-        // for(String str:strs){
-        //     char[] arr = str.toCharArray();
-        //     Arrays.sort(arr);
-        //     String key = new String(arr);
-        //     map.putIfAbsent(key,new ArrayList<>());
-        //     map.get(key).add(str);
-        // }
-        // return new ArrayList<>(map.values());
-        Map<String,List>map = new HashMap<>();
-        for(String s:strs){
-            int[] count = new int[26];
-            for(char c: s.toCharArray()){
-                count[c-'a']++;
-            }
-            StringBuilder sb = new StringBuilder();
-            for(int i:count){
-                sb.append("#");
-                sb.append(i);
-            }
-            String key = sb.toString();
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str:strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(str);
         }
-        return new ArrayList(map.values());
+        return new ArrayList<>(map.values());
+
+
+        // Map<String,List>map = new HashMap<>();
+        // for(String s:strs){
+        //     int[] count = new int[26];
+        //     for(char c: s.toCharArray()){
+        //         count[c-'a']++;
+        //     }
+        //     StringBuilder sb = new StringBuilder();
+        //     for(int i:count){
+        //         sb.append("#");
+        //         sb.append(i);
+        //     }
+        //     String key = sb.toString();
+        //     map.putIfAbsent(key, new ArrayList<>());
+        //     map.get(key).add(s);
+        // }
+        // return new ArrayList(map.values());
     }
 }
